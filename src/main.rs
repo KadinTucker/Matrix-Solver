4a84d83d0542530dbab5389a71982ac540e3b61c
KadinTucker <kadintucker@gmail.com>
2021-02-28 23:35:45 +0100

    Refactored matrix data representation and implemented row elimination function

--- src/main.rs
+++ src/main.rs
@@ -3,77 +3,79 @@ use std::str;
 fn main() {
     println!("Hello, world.");
 
-    let m = create_matrix(vec![vec![1.0, 2.0], vec![4.0, 3.0]]);
+    let mut m = create_matrix(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0], 3, 3).unwrap();
 
-    println!("{}", access(&m, 1, 0).unwrap());
+    // println!("{}", access(&m, 1, 0).unwrap());
 
-    let v1 = vec![2.0, 3.0, 4.0];
-    let v2 = vec![5.0, 2.0, 6.0];
-    let v3 = vec![4.0, 3.0];
+    // let v1 = vec![2.0, 3.0, 4.0];
+    // let v2 = vec![5.0, 2.0, 6.0];
+    // let v3 = vec![4.0, 3.0];
 
-    println!("{}", vector_to_string(&add_vectors(&v1, &v2).unwrap()));
+    // println!("{}", vector_to_string(&add_vectors(&v1, &v2).unwrap()));
 
-    println!("{}", matrix_to_string(&m));
+    // println!("{}", matrix_to_string(&m));
+
+    print_matrix(&m);
+
+    println!("");
+
+    m = eliminate(m, 0, 1, 0);
+
+    print_matrix(&m);
 
 }
 
 struct Matrix {
     nrows: usize,
     ncols: usize,
-    values: Vec<Vec<f64>>,
+    values: Vec<f64>,
 }
 
-fn create_matrix(matrix: Vec<Vec<f64>>) -> Matrix {
-    let ncols = match matrix.get(0) {
-        Some(T) => T.len(),
-        None => 0
-    };
-    Matrix {
-        nrows: matrix.len(),
-        ncols: ncols,
-        values: matrix
-    }
-}
-
-fn vector_to_string(vector: &Vec<f64>) -> String {
-    let mut s: String = String::from("[ ");
-    for element in vector.iter() {
-        s += &(element.to_string() + " ");
-    }
-    s += "]";
-    s
-}
-
-fn matrix_to_string(matrix: &Matrix) -> String {
-    let mut s: String = String::new();
-    for row in matrix.values.iter() {
-        s += &(vector_to_string(&row) + "\n");
-    }
-    s
-}
-
-fn access(matrix: &Matrix, row: usize, col: usize) -> Option<&f64> {
-    match matrix.values.get(row) {
-        Some(T) => T.get(col),
-        None => None
+fn create_matrix(values: Vec<f64>, nrows: usize, ncols: usize) -> Option<Matrix> {
+    let elts = values.len();
+    match nrows * ncols {
+        elts => Some(Matrix {
+            nrows: nrows,
+            ncols: ncols,
+            values: values
+        }),
+        _ => None
     }
 }
 
-fn scale_vector(vector: &Vec<f64>, scalar: f64) -> Vec<f64> {
-    let mut scaled: Vec<f64> = Vec::new();
-    for element in vector.iter() {
-        scaled.push(*element * scalar);
+fn print_matrix(matrix: &Matrix) {
+    for i in 0..matrix.nrows {
+        print!("[ ");
+        for j in 0..matrix.ncols {
+            print!("{} ", matrix.values[i * matrix.nrows + j]);
+        }
+        println!("]");
     }
-    scaled
 }
 
-fn add_vectors(vector1: &Vec<f64>, vector2: &Vec<f64>) -> Option<Vec<f64>> {
-    if vector1.len() != vector2.len() {
-        ()
+fn scale_row(mut matrix: Matrix, row: usize, scalar: f64) -> Matrix {
+    if row < matrix.nrows {
+        for i in (row * matrix.ncols)..(row * matrix.ncols + matrix.ncols) {
+            matrix.values[i] *= scalar;
+        }
     }
-    let mut sum: Vec<f64> = Vec::new();
-    for index in 0..vector1.len() {
-        sum.push(vector1.get(index).unwrap() + vector2.get(index).unwrap());
+    matrix
+}
+
+fn add_and_scale(mut matrix: Matrix, origin_row: usize, dest_row: usize, scalar: f64) -> Matrix {
+    if origin_row < matrix.nrows && dest_row < matrix.nrows {
+        for i in 0..matrix.ncols {
+            matrix.values[i + dest_row * matrix.ncols] += matrix.values[i + origin_row * matrix.ncols] * scalar;
+        }
     }
-    Some(sum)
+    matrix
+}
+
+fn add_rows(mut matrix: Matrix, origin_row: usize, dest_row: usize) -> Matrix {
+    add_and_scale(matrix, origin_row, dest_row, 1.0)
+}
+
+fn eliminate(mut matrix: Matrix, origin_row: usize, dest_row: usize, dest_col: usize) -> Matrix {
+    let elim_scalar = -matrix.values[matrix.ncols * dest_row + dest_col] / matrix.values[matrix.ncols * origin_row + dest_col];
+    add_and_scale(matrix, origin_row, dest_row, elim_scalar)
 }
